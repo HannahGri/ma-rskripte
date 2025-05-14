37d786539aa98ba7f61ded24f5b5017334adf6d3
HannahGri <hannah.grigoleit@gmail.com>
2025-05-14 13:35:05 +0200

    Update Aufbau MixNorm-Netz.R

--- Neuronale Netze/Aufbau MixNorm-Netz.R
+++ Neuronale Netze/Aufbau MixNorm-Netz.R
@@ -1,44 +1,75 @@
+#Datensatz simulieren, ohne Korrelationen
+n_sample <- 100000
 
-df_data <- Verknüpfung_Bestand_gefahren%>%
-  select(
-    Jahr=F002_Jahr,
-    HGK =ZUERS_HGK,
-    SGK =ZUERS_SGK,
-    Betr = F103_Betriebsart,
-    Alter_kat = Alter_kat,
-    VS_kat = VS_kat,
-    qm_kat = qm_kat,
-    BAKL = F044_BAKL_Priv,
-    AVB = F050_AVB_VG,
-    UVVZ = F054_UVVZ,
-    Rohbau = F183b_Rohbau,
-    Feu = F030_Feuer,
-    LW = F032_LW,
-    ST = F033_ST,
-    EL = F035_EL,
-    gew = F077_gew_Nutzung,
-    Vorschaden_feuer=Vorschaden_feuer,
-    Vorschaden_lw=Vorschaden_lw,
-    Vorschaden_sturm=Vorschaden_sturm,
-    Vorschaden_el=Vorschaden_el,
-    Vorschaden_sonst=Vorschaden_sonst,
-    y_feuer = y_feuer,
-    y_lw = y_lw,
-    y_sturm = y_sturm,
-    y_el = y_el,
-    y_sonst = y_sonst
-  )  %>% mutate(
-    across(-c(y_feuer, y_lw, y_sturm, y_el, y_sonst,
-              VS_kat,Alter_kat, qm_kat, Jahr), as.factor),
-    y_feuer= ifelse(y_feuer<=1, 0L, log10(y_feuer)),
-    y_lw= ifelse(y_lw<=1, 0L, log10(y_lw)),
-    y_sturm= ifelse(y_sturm<=1, 0L, log10(y_sturm)),
-    y_el= ifelse(y_el<=1, 0L, log10(y_el)),
-    y_sonst= ifelse(y_sonst<=1, 0L, log10(y_sonst))) %>%
-  mutate(SchNr = (1:nrow(Verknüpfung_Bestand)),
-         Alter_kat = ifelse(is.na(Alter_kat), 0, Alter_kat)) %>%
-  filter(!is.na(UVVZ)) %>%
-  select(-y_sonst, -Vorschaden_sonst)
+set.seed(111)
+Jahr_sim <- sample(2014:2023 , size = n_sample, replace = TRUE, prob = as.numeric(prop.table(table(df_data$Jahr))))
+HGK_sim <- sample(0:4 , size = n_sample, replace = TRUE, prob = c(0.04, 0.87, 0.07,0.01, 0.01))
+SGK_sim <- sample(0:3 , size = n_sample, replace = TRUE, prob = c(0.31, 0.16, 0.46, 0.07))
+Betr_sim <- sample(c(00000, 00051, 00052, 00053, 00055, 00059) , size = n_sample, replace = TRUE, prob = c(0.002, 0.03, 0.03, 0.8, 0.128, 0.01))
+Alter_kat_sim <- sample(0:61 , size = n_sample, replace = TRUE, prob = dnorm(0:61, mean = 0, sd=15) / sum(dnorm(0:61, mean = 0, sd=20)) )
+VS_kat_sim <- sample(0:57 , size = n_sample, replace = TRUE, prob = dnorm(0:57, mean = 23, sd = 7)/ sum(dnorm(0:57, mean = 23, sd = 7)))
+qm_kat_sim <- sample(0:50 , size = n_sample, replace = TRUE, prob = c(0.45, 0.65* dnorm(1:50, mean = 6, sd = 2)/sum(dnorm(1:50, mean = 6, sd = 2))))
+BAKL_sim <- sample(0:3 , size = n_sample, replace = TRUE, prob = c(0.01, 0.95, 0.03, 0.01))
+AVB_sim <- sample(c(00, 01, 02, 03, 06, 12, 14, 15, 98) , size = n_sample, replace = TRUE, prob =c(0.0002, 0.004, 0.0008, 0.005, 0.24, 0.3, 0.16, 0.03, 0.26))
+UVVZ_sim <- sample(c(1,2) , size = n_sample, replace = TRUE, prob =c(0.3, 0.7))
+Rohbau_sim <- sample(c(1,2) , size = n_sample, replace = TRUE, prob = c(0.003, 0.997))
+LW_sim <- sample(c(1,2), size = n_sample, replace = TRUE, prob = c(0.95, 0.05))
+Feu_sim <- sample(c(1,2) , size = n_sample, replace = TRUE, prob = c(0.96, 0.04))
+ST_sim <- sample(c(1,2) , size = n_sample, replace = TRUE, prob = c(0.97, 0.03))
+EL_sim <- sample(c(1,2,3,5) , size = n_sample, replace = TRUE, prob = c(0.24, 0.51, 0.248, 0.002))
+gew_sim <- sample(c(1,2,9) , size = n_sample, replace = TRUE, prob = c(0.02, 0.001, 0.979))
+Vorschaden_lw_sim <- sample(c(0,1) , size = n_sample, replace = TRUE, prob = c(0.9, 0.1))
+Vorschaden_feuer_sim <- sample(c(0,1) , size = n_sample, replace = TRUE, prob = c(0.98, 0.02))
+Vorschaden_sturm_sim <- sample(c(0,1) , size = n_sample, replace = TRUE, prob = c(0.9, 0.1))
+Vorschaden_el_sim <- sample(c(0,1) , size = n_sample, replace = TRUE, prob = c( 0.99, 0.01))
+
+y_lw_sim <-  dist_mixture(dists = list(dist_dirac(0),
+                                       dist_normal(mean= 3.2, sd=0.57)),
+                          probs = list(0.958,0.042))$sample(n_sample)
+
+y_sturm_sim <-  dist_mixture(dists = list(dist_dirac(0),
+                                       dist_normal(mean= 2.9, sd=0.51)),
+                          probs = list(0.965,0.035))$sample(n_sample)
+y_feuer_sim <-  dist_mixture(dists = list(dist_dirac(0),
+                                       dist_normal(mean= 3.13, sd=0.76)),
+                          probs = list(0.995,0.005))$sample(n_sample)
+y_el_sim <-  dist_mixture(dists = list(dist_dirac(0),
+                                          dist_normal(mean= 3.66, sd=0.7)),
+                             probs = list(0.997,0.003))$sample(n_sample)
+
+
+
+data_sim <- tibble(
+  Jahr = Jahr_sim,
+  HGK = HGK_sim,
+  SGK = SGK_sim,
+  Betr = Betr_sim,
+  Alter_kat = Alter_kat_sim,
+  VS_kat = VS_kat_sim,
+  qm_kat = qm_kat_sim,
+  BAKL = BAKL_sim,
+  AVB = AVB_sim,
+  UVVZ = UVVZ_sim,
+  Rohbau = Rohbau_sim,
+  LW = LW_sim,
+  Feu = Feu_sim,
+  ST = ST_sim,
+  EL = EL_sim,
+  gew = gew_sim,
+  Vorschaden_lw = Vorschaden_lw_sim,
+  Vorschaden_sturm = Vorschaden_sturm_sim,
+  Vorschaden_feuer = Vorschaden_feuer_sim,
+  Vorschaden_el = Vorschaden_el_sim,
+  y_lw = y_lw_sim,
+  y_sturm = y_sturm_sim,
+  y_feuer = y_feuer_sim,
+  y_el = y_el_sim
+)  %>% mutate(
+  across(-c(y_feuer, y_lw, y_sturm, y_el,
+            VS_kat,Alter_kat, qm_kat, Jahr), as.factor)) %>%
+  mutate(Nr = (1:n_sample))
+
+  
 
 
 # Aufteilung in Test-& Trainingsdaten
@@ -55,7 +86,7 @@ test_data<- df_data%>%
 test_features<- test_data%>% select(-c(y_feuer,
                                        y_lw,
                                        y_sturm, y_el,
-                                       SchNr))
+                                       Nr))
 
 test_Y<- test_data%>% select(y_feuer, y_lw, y_sturm, y_el) 
 
@@ -64,7 +95,7 @@ train_data<- df_data%>% anti_join(test_data)
 train_features<- train_data%>% select(-c(y_feuer,
                                          y_lw,
                                          y_sturm, y_el,
-                                         SchNr))
+                                         Nr))
 train_Y<- train_data%>% select(y_feuer, y_lw, y_sturm, y_el) 
 
 
