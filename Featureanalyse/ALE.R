c16f681326e927173a2657b1e41a46a6347da961
HannahGri <hannah.grigoleit@gmail.com>
2025-05-15 10:48:43 +0200

    Update ALE.R

--- Featureanalyse/ALE.R
+++ Featureanalyse/ALE.R
@@ -1,7 +1,7 @@
 
 library(iml)
 
-# VS, Alter, Fläche, HGK, SGK werden in der Reihenfolge behalten
+# VS, Alter, Fläche, HGK, SGK werden in der Reihenfolge sortiert
 
 predictor_lw <- Predictor$new(model=nn_model_lw$model, 
                               data = train_features %>%mutate(VS_kat=factor(VS_kat,ordered=TRUE),
@@ -11,63 +11,72 @@ predictor_lw <- Predictor$new(model=nn_model_lw$model,
                                      SGK=factor(SGK,ordered = TRUE)),
                            y=train_Y$y_lw)
 
-
+FI_ALE_Betr_lw<- FeatureEffect$new(predictor_lw, feature="Betr", method="ale")
 FI_ALE_Jahr_lw<- FeatureEffect$new(predictor_lw, feature="Jahr", method="ale")
 FI_ALE_VS_lw<- FeatureEffect$new(predictor_lw, feature="VS_kat", method="ale")
 
-# Plot 1: Jahr
-
-ggplot(data=FI_ALE_Jahr_lw$results%>% filter(.class==1))+
-  geom_col(aes(x=(factor(Jahr) 
-  ),
-  y= .value), fill="skyblue3",alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name=NULL ,breaks=2014:2023)+
-  labs(y=NULL,
-       title=expression(alpha))+
-ggplot(data=FI_ALE_Jahr_lw$results%>% filter(.class==3))+
-  geom_col(aes(x=factor(Jahr 
-  ),
-  y= .value), fill="skyblue3", alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name=NULL ,breaks=2014:2023)+
-  labs( y=NULL,
-        title= expression(mu))+
-ggplot(data=FI_ALE_Jahr_lw$results%>% filter(.class==4))+
-  geom_col(aes(x=factor(Jahr 
-  ),
-  y= .value), fill="skyblue3", alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name="Jahr" ,breaks=2014:2023)+
-  labs( y=NULL,
-        title= expression(sigma))+
+# Plot Betriebsart
+ggplot(data=  FI_ALE_Betr_lw$results %>% filter(.class == 1), aes(x = factor(Betr), y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_Betr_lw$results %>% filter(.class == 3) , aes(x = factor(Betr), y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_Betr_lw$results %>% filter(.class == 4) , aes(x = factor(Betr), y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = "Betriebsart", y = NULL) +
   plot_layout(ncol=1)
 
-# Plot 2: Verischerungssumme (kategorisiert)
 
-ggplot(data=FI_ALE_VS_lw$results%>% filter(.class==1))+
-  geom_col(aes(x=(factor(VS_kat) 
-  ),
-  y= .value), fill="skyblue3",alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name=NULL ,breaks=c(seq(0,55, by=5)))+
-  labs(x="", y="ALE",
-       title=expression(alpha))+
-ggplot(data=FI_ALE_VS_lw$results%>% filter(.class==3))+
-  geom_col(aes(x=(VS_kat 
-  ),
-  y= .value), fill="skyblue3", alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name=NULL ,breaks=c(seq(0,55, by=5)))+
-  labs(x="", y="ALE",
-       title= expression(mu))+
-ggplot(data=FI_ALE_VS_lw$results%>% filter(.class==4))+
-  geom_col(aes(x=(VS_kat 
-  ),
-  y= .value), fill="skyblue3", alpha=0.6)+
-  geom_hline(yintercept = 0)+
-  scale_x_discrete(name="Versicherungssumme (kateg.)", breaks=c(seq(0,55, by=5)))+
-  labs(x=NULL, , y="ALE",
-       title= expression(sigma))+
-  plot_layout(ncol=1)
+# Plot Jahr
+ggplot(data=  FI_ALE_Jahr_lw$results %>% filter(.class == 1), aes(x =Jahr, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_Jahr_lw$results %>% filter(.class == 3) , aes(x = Jahr, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_Jahr_lw$results %>% filter(.class == 4) , aes(x =Jahr, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = "Jahr", y = NULL) +
+plot_layout(ncol=1)
+
+# Plot Versicherungssumme
+ggplot(data=  FI_ALE_VS_lw$results %>% filter(.class == 1), aes(x = VS_kat, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_discrete(breaks= seq(0,53, by=5))+
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_VS_lw$results %>% filter(.class == 3) , aes(x = VS_kat, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_discrete(breaks= seq(0,53, by=5))+
+  labs(title = NULL, x = NULL, y = NULL) +
+  
+ggplot(data=   FI_ALE_VS_lw$results %>% filter(.class == 4) , aes(x = VS_kat, y = .value, group=1)) +
+  geom_point()+
+  geom_line(size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title = NULL, x = "VS (kateg.)", y = NULL) +
+  scale_x_discrete(breaks= seq(0,53, by=5))+
+plot_layout(ncol=1)
+
 
