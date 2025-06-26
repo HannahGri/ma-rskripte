682964ad07effc1e8170e91efec016bb239ad0fa
HannahGri <hannah.grigoleit@gmail.com>
2025-06-26 13:40:28 +0200

    Update ALE.R

--- 04 Featureanalyse/ALE.R
+++ 04 Featureanalyse/ALE.R
@@ -2,9 +2,10 @@
 
 # VS, Alter, Fläche, HGK, SGK werden in der Reihenfolge sortiert
 predictor_lw <- Predictor$new(model=nn_model_lw$model, 
-                              data = train_features %>%mutate(VS_kat=factor(VS_kat,ordered=TRUE),
+                              data = train_features %>% mutate(VS_kat=factor(VS_kat,ordered=TRUE),
                                      Alter_kat=factor(Alter_kat,ordered=TRUE),
                                      qm_kat=factor(qm_kat,ordered=TRUE),
+                                     Jahr=factor(Jahr,ordered = TRUE),
                                      HGK=factor(HGK,ordered = TRUE),
                                      SGK=factor(SGK,ordered = TRUE)),
                            y=train_Y$y_lw)
@@ -39,18 +40,21 @@ ggplot(data=  FI_ALE_Jahr_lw$results %>% filter(.class == 1), aes(x =Jahr, y = .
   geom_point()+
   geom_line(linewidth = 0.5) +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_discrete(labels= 2014:2023)+
   labs(title = NULL, x = NULL, y = NULL) +
   
 ggplot(data=   FI_ALE_Jahr_lw$results %>% filter(.class == 3) , aes(x = Jahr, y = .value, group=1)) +
   geom_point()+
   geom_line(linewidth = 0.5) +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_discrete(labels= 2014:2023)+
   labs(title = NULL, x = NULL, y = NULL) +
   
 ggplot(data=   FI_ALE_Jahr_lw$results %>% filter(.class == 4) , aes(x =Jahr, y = .value, group=1)) +
   geom_point()+
   geom_line(linewidth = 0.5) +
   geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_x_discrete(labels= 2014:2023)+
   labs(title = NULL, x = "Jahr", y = NULL) +
 plot_layout(ncol=1)
 
